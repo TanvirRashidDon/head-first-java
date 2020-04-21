e0b3bb6e9666537500a6444b7b09d3ac2e98e3d6
Mumtahina Faguni <tanvirrashiddon@gmail.com>
2020-04-22 03:47:52 +0600

    SimpleDotGame for 1D arry

--- src/fifth_chapter/simpledotcom/SimpleDotCom.java
+++ src/fifth_chapter/simpledotcom/SimpleDotCom.java
@@ -2,14 +2,31 @@ package fifth_chapter.simpledotcom;
 
 public class SimpleDotCom {
     private int [] locationCells;
-    private int nuberOfHits = 0;
+    private int nuberOfHits; // 0 is default value for numeric instance variable
 
     public void setLocationCells(int[] locationCells) {
         this.locationCells = locationCells;
     }
 
     public String checkYourself(String userGuss){
-        String result = "hit";
+        int guess = Integer.parseInt(userGuss);
+
+        String result = "miss";
+
+        for(int cell : locationCells){
+            if(guess ==  cell) {
+                result = "hit";
+                nuberOfHits += 1;
+                break;
+            }
+        }
+
+        if(nuberOfHits == locationCells.length){
+            result = "kill";
+        }
+
+        System.out.println(result);
+
         return result;
     }
 }
--- src/fifth_chapter/simpledotcom/SimpleDotComTestDrive.java
+++ src/fifth_chapter/simpledotcom/SimpleDotComTestDrive.java
@@ -2,22 +2,30 @@ package fifth_chapter.simpledotcom;
 
 public class SimpleDotComTestDrive {
     public void doSomething(){
+        int numOfGuess = 0;
         SimpleDotCom simpleDotCom = new SimpleDotCom();
+        GameHelper helper = new GameHelper();
 
-        int [] locations = {1, 3, 5};
+        int randomNumber = (int) (Math.random() * 5);
+        int [] locations = {randomNumber, randomNumber + 1, randomNumber + 2};
+
         simpleDotCom.setLocationCells(locations);
 
-        String userGuess = "2";
+        boolean isAlive = true;
 
-        String result = simpleDotCom.checkYourself(userGuess);
+        while (isAlive) {
+            String userGuess = helper.getUserInput("Entet a number : ");
 
-        String testResult = "failed";
+            String result = simpleDotCom.checkYourself(userGuess);
 
-        if(result.equals("hit")){
-            testResult = "Pass";
+            numOfGuess++;
+
+            if (result.equals("kill")) {
+                isAlive = false;
+                System.out.println("You took "  + numOfGuess + " guesses");
+            }
+            //System.out.println(testResult);
         }
-
-        System.out.println(testResult);
     }
 
 }
