291d756fdf6dde36c7d53ac2b0218271de8b2233
Mumtahina Faguni <tanvirrashiddon@gmail.com>
2020-05-08 00:39:25 +0600

    call Subclass behavour by Superclass

--- src/seventh_chapter/inheritance/AnimalTestDrive.java
+++ src/seventh_chapter/inheritance/AnimalTestDrive.java
@@ -5,6 +5,10 @@ import seventh_chapter.inheritance.animals.Hippopotamus;
 public class AnimalTestDrive {
 
     public void doSomething(){
+        callSubbclassBehavourBySuperclass();
+    }
+
+    public void callSubbclassBehavourBySuperclass(){
         Animal animal = new Hippopotamus(   );
         animal.makeNoise();
         animal.eat();
